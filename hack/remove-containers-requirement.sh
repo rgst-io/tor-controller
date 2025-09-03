841fb38a1f66f0095a9b1d1ea0219a9dc0221ab7
Jared Allard <jaredallard@users.noreply.github.com>
2025-09-03 11:07:17 -0700

    chore: remove install.yaml

--- hack/remove-containers-requirement.sh
+++ hack/remove-containers-requirement.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
+#!/usr/bin/env bash 
+set -euo pipefail
 
 CRD_DIR="$1"
 find "$CRD_DIR" -type f -exec sh -c 'TEMP_FILE=`mktemp` && grep -v -- "- containers" "$0" > "$TEMP_FILE" && mv "$TEMP_FILE" "$0"' "{}" \;
